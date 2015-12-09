75185bd18d28ba00a8e6eb09ae861dceb5ad2323
Jake Klingler <thearrow3456@gmail.com>
2015-12-09 14:15:52 -0700

    super basic functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
 (function() {
   'use strict';
+
+  var buttons = document.querySelectorAll('.button');
+  var screen = document.querySelector('.screen');
+
+  var buttonClick = function() {
+    var buttonText = this.innerHTML;
+
+    if (buttonText === 'C') {
+      screen.innerHTML = '';
+    } else if (buttonText === '=') {
+      screen.innerHTML = eval(screen.innerHTML);
+    } else {
+      screen.innerHTML += buttonText;
+    }
+  };
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', buttonClick);
+  }
 })();
